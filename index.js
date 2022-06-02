4b0c86ba8070191571345c76b740565f7faf24ec
NicoVulcan <giraldojnicolas@gmail.com>
2022-06-02 18:18:47 -0500

    heroku

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const {routerApi} = require("./routes");
 const { logErrors, errorHandler, boomErrorHandler } = require("./middlewares/errorHandler");
 
 const app = express();
-const port = 3023;
+const port = 3024;
 
 app.use(express.json());
 
-const whitelist = ["http://localhost:3023", "https://myapp.co"];
+const whitelist = ["http://localhost:3024", "https://myapp.co"];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
